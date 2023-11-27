29f4364d8dced6e6eb71e244bdc63bec62530060
excalibur-Leo <1224896002@qq.com>
2023-11-27 19:23:28 +0800

    调整logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -40,7 +40,7 @@ const onClick = route => {
     .logo { 
         width: 90px;
         height: 90px;
-        background: url(../assets/LOGO.jpg) no-repeat;
+        background: url(../assets/logo2.jpg) no-repeat;
         background-size: contain;
     }
 
